b840beca7f440b3fdc4478d0268a7493144e2364
2sick2 <toonit772@gmail.com>
2025-04-19 01:16:40 +0900

    Initial Commit

---
+++ SttProgram/SttProgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SttProgramApp.swift
+//  SttProgram
+//
+//  Created by Yong-Won Lee on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SttProgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SttProgramTests/SttProgramTests.swift
@@ -0,0 +1,17 @@
+//
+//  SttProgramTests.swift
+//  SttProgramTests
+//
+//  Created by Yong-Won Lee on 4/19/25.
+//
+
+import Testing
+@testable import SttProgram
+
+struct SttProgramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
